5ddee48ea9ef02e265b9f6e67b66923d176998d1
Regiane-Martins <regianegmartins@hotmail.com>
2023-03-24 09:34:41 -0300

    refazendo chamada tela page emptycart

--- src/layout/empty-cart/index.js
+++ src/layout/empty-cart/index.js
@@ -2,12 +2,14 @@ import { Container } from "../container";
 import * as s from "./styled";
 
 function EmptyCart(props) {
-    const {changeScreen} = props
+  const { changeScreen } = props;
   return (
     <Container>
       <s.Content>
         <s.Title>Ops, seu carrinho está vazio.</s.Title>
-        <s.Link href="/" onClick={(e)=> changeScreen=("default")}>Escolha seus produtos</s.Link>
+        <s.Link href="/" onClick={(e) => changeScreen("default")}>
+          Escolha seus produtos
+        </s.Link>
       </s.Content>
     </Container>
   );
